766f9d9bde0fffb2150f55d7afe0ddf3ef0d6a75
julieta M <julietyartes2@gmail.com>
2022-04-02 12:00:50 -0500

    arhivo de estudiantes

---
+++ script.js
@@ -0,0 +1,9 @@
+grupoEstudiantes=prompt(" estudiantes del grupo");
+hombres=prompt("ingrese elnumero de hombres");
+mujeres=prompt("ingrese el numero de mujeres");
+
+totalHombres=(grupoEstudiantes*hombres)/100
+document.write("el total de porcenteje de Hombres:",totalHombres,"%");
+document.write("<br>");
+totalmujeres=(grupoEstudiantes*mujeres)/100
+document.write("el total de porcentaje en mujeres:",totalmujeres,"%");
